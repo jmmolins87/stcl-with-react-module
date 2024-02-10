b11f2bfc08c1708661572d393a213eb120447a19
jmmcDesign <35576283+jmmolins87@users.noreply.github.com>
2024-02-10 14:33:49 +0100

    create module component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,51 @@
-import {LitElement, html} from 'lit-element';
+import { LitElement, html, css } from "lit-element";
 
-class MyElement extends LitElement {
-    render() {
-      return html`
-        <h1>Hello from MyElement!</h1>
-      `;
-    }
+class Button extends LitElement {
+  constructor() {
+    super();
   }
-  customElements.define('my-element', MyElement);  
+  static get properties() {
+    return {
+      text: {
+        Type: String,
+        attribute: true,
+      },
+    };
+  }
+  static get styles() {
+    return css`
+      button {
+        all: initial;
+        background-color: #9292b5;
+        font-family: cursive;
+        margin-right: 20px;
+        width: 150px;
+        border-radius: 10px;
+        text-align: center;
+        padding: 2px;
+        cursor: pointer;
+      }
+    `;
+  }
+  firstUpdated() {
+    this.button = this.shadowRoot.querySelector("button");
+  }
+  mouseDownListener(event) {
+    this.button.style.backgroundColor = "#585866";
+  }
+  mouseUpListener(event) {
+    this.button.style.backgroundColor = "#9292b5";
+  }
+  render() {
+    return html`
+      <button
+        @mousedown="${this.mouseDownListener}"
+        @mouseup="${this.mouseUpListener}"
+      >
+        ${this.text}
+      </button>
+    `;
+  }
+}
+
+window.customElements.define("wc-button", Button);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   devtool: 'source-map',
@@ -7,6 +6,5 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
-  },
-  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+  }
 };
