9ff4d93c8eb221f9a018a7509c851a765539863b
jmmcDesign <35576283+jmmolins87@users.noreply.github.com>
2024-02-10 13:16:44 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import {LitElement, html} from 'lit-element';
+
+class MyElement extends LitElement {
+    render() {
+      return html`
+        <h1>Hello from MyElement!</h1>
+      `;
+    }
+  }
+  customElements.define('my-element', MyElement);  
